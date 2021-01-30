716318e9044aacffbfc6d063a0f9594d4261bdf6
Trevor Hinkle <trevor.hinkle@tmrow.com>
2021-01-30 11:47:22 +0100

    first edits

---
+++ www/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => (
+  <>
+    <h1>About</h1>
+    <p>Climate change is an overwhelming problem. It touches every person, every industry, every company, and every country. The causes, solutions, and implications are complex. It's no wonder that many of us find it easier to avoid or deny the problem.</p>
+    <p>
+      We can never hope to completely make sense of climate change, but that doesn't mean we can't try. The mission of Mental Models for Climate Change is to curate ideas and frameworks for making sense of climate change, with the hope that sharing
+       and honing these ideas will help us approach the issue with clearer thinking, objectivity, and confidence.
+    </p>
+    <p>
+      Mental Models for Climate Change is curated by <a href="https://twitter.com/trevorhinkle3">Trevor Hinkle</a>, but is meant to be a collaborative effort. Everyone comes to this issue of climate change with their own unique perspective, and this project
+      will be most successful if it can aggregate and amplify a diverse range of voices and perspectives.
+    </p>
+  </>
+)
+
+export default About
---
+++ www/src/components/Bio.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Follow } from 'react-twitter-widgets'
+import styles from './Bio.module.scss'
+
+const Bio = ({ config, expanded }) => (
+  <>
+    <img
+      className={styles.avatar}
+      src={config.userAvatar}
+      alt={config.userName}
+    />
+    <p>
+      Written by <strong>{config.userName}</strong>
+      {` `}
+      <Follow
+        username={config.userTwitter}
+        options={{ count: expanded ? true : 'none' }}
+      />
+    </p>
+  </>
+)
+
+export default Bio
